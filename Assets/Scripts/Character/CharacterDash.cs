93fc3043a93b14849aad9bf48694192eed82cd8a
windupstill <windupknob@gmail.com>
2023-08-26 14:23:08 +0900

    [FIx] : 공중연속대쉬 불가능
    공중연속대쉬 불가능기능 추가

--- Assets/Scripts/Character/CharacterDash.cs
+++ Assets/Scripts/Character/CharacterDash.cs
@@ -5,33 +5,50 @@ using UnityEngine.InputSystem;
 
 public class CharacterDash : MonoBehaviour
 {
+    #region PublicVariables
     public float m_dashVelocity = 50f;
     public float m_accelerationDuration = 0.05f;
     public float m_decelerationDuration = 0.3f;
     public float m_dashDuration = 0.5f;
-    private Rigidbody rb;
+    #endregion
 
+    #region PrivateVariables
+    private Rigidbody rb;
     private float m_cameraDis = 10f;
     private float m_plus = 0.1f;
+    private bool m_jumpDashCount = false;
+    #endregion
 
-    private void Start()
+    #region PublicMethod
+    public void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    public void Update()
+    {
+        if (!CharacterManager.instance.GetIsJump())
+        {
+            m_jumpDashCount=true;
+        }
+    }
+
     public void OnDash(InputAction.CallbackContext context)
     {
-        if (context.started && !CharacterManager.instance.GetIsDash())
+        if (context.started && !CharacterManager.instance.GetIsDash() && m_jumpDashCount)
         {
             CharacterManager.instance.SetIsDash(true);
             CharacterManager.instance.SetCanMove(false);
             Vector3 dashDirection = transform.forward;
+            m_jumpDashCount = false;
             rb.velocity = Vector3.zero; // 초기 속도 초기화
-            StartCoroutine(IE_mDash(dashDirection));
+            StartCoroutine(IE_mDash(dashDirection));            
         }
         
     }
+    #endregion
 
+    #region PrivateMethod   
     private IEnumerator IE_mDash(Vector3 dashDirection)
     {
         float startTime = Time.time;
@@ -56,4 +73,5 @@ public class CharacterDash : MonoBehaviour
         CharacterManager.instance.SetCanMove(true);
 
     }
+    #endregion
 }
