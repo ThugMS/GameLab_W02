57aea823e170ff7b367117d2c8464f83a013c2bb
MinseokLee <leems041625@gmail.com>
2023-08-28 20:42:36 +0900

    [Fix] : 대쉬 후 자동으로 이동되는 오류 수정

--- Assets/Scripts/Character/CharacterDash.cs
+++ Assets/Scripts/Character/CharacterDash.cs
@@ -82,6 +82,7 @@ public class CharacterDash : MonoBehaviour
         rb.velocity = Vector3.zero; // 동작 종료 시 속도 초기화
         CharacterManager.instance.SetIsDash(false); 
         CharacterManager.instance.SetCanMove(true);
+        CharacterManager.instance.SetIsMove(false);
     }
     #endregion
 }
